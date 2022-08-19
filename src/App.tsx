53d53ef3072b02d09c1141f403670ce3b9f4df59
Ben Wittenberg <benjaminwittenberg@gmail.com>
2022-08-18 22:26:41 -0400

    add two versions of sentry and create two clients

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as Sentry from "@sentry/browser";
+import * as Sentry2 from "sentry2";
+
+declare global {
+  interface Window {
+    __SENTRY__: any;
+  }
+}
+
+Sentry.init({
+  dsn: "https://123@sentry.io/456",
+  integrations: [new Sentry.Dedupe()],
+  defaultIntegrations: false,
+  debug: true,
+  beforeSend(event) {
+    console.log("customer sentry", event);
+    return null;
+  },
+});
+
+const client1 = new Sentry2.BrowserClient({
+  dsn: "https://123@sentry.io/123",
+  integrations: [new Sentry2.Integrations.Dedupe()],
+  beforeSend(event) {
+    console.log("js client sentry", event);
+    return null;
+  },
+});
+
+new Sentry2.Hub(client1);
+
+console.log(
+  "There should only be one integration, but there are two.",
+  window.__SENTRY__.globalEventProcessors
+);
+
+Sentry.captureException(new Error("error!"));
 
 function App() {
   return (
